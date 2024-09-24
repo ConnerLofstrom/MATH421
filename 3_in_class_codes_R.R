134ba9d30e5f0096f7ef109754b1ea39219982f7
ConnerLofstrom <144947005+ConnerLofstrom@users.noreply.github.com>
2024-09-24 10:36:34 -0400

    commit index.qmd

--- 3_in_class_codes_R.R
+++
@@ -1,9 +0,0 @@
-#Read in a csv file
-df <-- read.csv("https://covid19.who.int/WHO-COVID-19-global-data.csv")
-
-#Show the names of the columns
-names(df)
-
-#Structure of the data in general
-str(df)
-
---
+++ Assignment 3/3_in_class_codes_R.R
@@ -0,0 +1,9 @@
+#Read in a csv file
+df <-- read.csv("https://covid19.who.int/WHO-COVID-19-global-data.csv")
+
+#Show the names of the columns
+names(df)
+
+#Structure of the data in general
+str(df)
+
